80cc5a040d1934563778936166991a7412adfc53
Charline <lch129444@hotmail.com>
2020-06-30 11:52:00 -0500

    Add for_counter_dictionary

---
+++ for_counter_dictionary.py
@@ -0,0 +1,8 @@
+book_title = ['great','expectation','the','adventures','of','great','expectation']
+word_counter = {}
+for word in book_title:
+    if word not in word_counter:
+        word_counter[word] = 1
+    else:
+        word_counter[word] += 1
+print(word_counter)
